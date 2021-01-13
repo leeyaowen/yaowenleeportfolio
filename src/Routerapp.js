0788c30e00595f615dcbf2c95ab72f6e7b09545b
leeyaowen <jcjh30101@gmail.com>
2021-01-13 16:11:07 +0800

    made changes

--- src/Routerapp.js
+++ src/Routerapp.js
@@ -6,7 +6,7 @@ import Home from './Home';
 function Routerapp() {
     return(
         <div className='router'>
-            <Router>
+            <Router basename='/wen-portfolio'>
                 <Switch>
                     <Route path='/' exact component={Home} />
                     <Route path='/Host' component={Host} />                
