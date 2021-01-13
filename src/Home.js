e91e3dd424bdcfe7b95db7bcedf3c714aa3602a8
leeyaowen <jcjh30101@gmail.com>
2021-01-14 00:31:54 +0800

    add cover photo

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,18 @@ function Home () {
     return(
         <div>
             <div className='home-div'>
-                <Carousel>
+                <Carousel className='coverloop'>
                     <Carousel.Item>
-                        <img className='w-100' src="https://photos.smugmug.com/photos/i-hv2VfRC/0/aea0fb80/X5/i-hv2VfRC-X5.jpg" alt="cover01"/>
+                        <img className='simg d-block mx-auto' src="https://photos.smugmug.com/photos/i-TSLNx7w/0/b0171c38/X5/i-TSLNx7w-X5.jpg" alt="cover01"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className='w-100' src="https://photos.smugmug.com/photos/i-d2g6TZ8/0/6ecd921f/X5/i-d2g6TZ8-X5.jpg" alt="cover02"/>
+                        <img className='simg d-block mx-auto' src="https://photos.smugmug.com/photos/i-d2g6TZ8/0/6ecd921f/X5/i-d2g6TZ8-X5.jpg" alt="cover02"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className='w-100' src="https://photos.smugmug.com/photos/i-TSLNx7w/0/b0171c38/X5/i-TSLNx7w-X5.jpg" alt="cover03"/>
+                        <img className='simg d-block mx-auto' src="https://photos.smugmug.com/photos/i-FgCzRLK/0/6beb2c4a/X5/i-FgCzRLK-X5.jpg" alt="cover03"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className='simg d-block mx-auto' src="https://photos.smugmug.com/photos/i-gkMsSf7/0/d3e6b70e/X5/i-gkMsSf7-X5.jpg" alt="cover04"/>
                     </Carousel.Item>
                 </Carousel>
             </div>
